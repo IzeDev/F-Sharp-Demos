024f4f40dead803782d93b14b4e57dfa5bf4e69b
IzeDev <jimmy.gustafsson@live.com>
2018-10-09 19:46:58 +0200

    Added another chapter

---
+++ ComposableFunctions/ComposableFunctions/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+// A simple add-function
+let add first second = first + second
+
+// Partial application of add:
+let addFive = add 5
+
+// composing functions
+let toUpper (text : string) = text.ToUpper()
+
+let addExclamation text = text + "!"
+
+let doShout = toUpper >> addExclamation
+
+[<EntryPoint>]
+let main argv =
+    // Usage of addFive
+    let fifteen = addFive 10
+
+    // Pipelining data through functions
+    let someValue = fifteen |> addFive |> addFive
+    printfn "%d" someValue
+    let greeting = "hello world"
+    let shoutGreeting = doShout greeting
+    printfn "%s" shoutGreeting
+    0 // return an integer exit code
