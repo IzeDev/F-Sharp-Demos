ce4fe9971e7c9bf92d007ca7ee17aea273298db5
IzeDev <jimmy.gustafsson@live.com>
2018-12-09 12:04:07 +0100

    Did another lesson

---
+++ Get Grogramming with F Sharp/05 Lesson 13 Higher Order Functions/Lesson 13 Higher Order Functions/Program.fs
@@ -0,0 +1,41 @@
+﻿open System.IO
+
+type Customer = { age : int}
+
+// A higher order function
+let where filter collection =
+    seq {
+        for item in collection do
+            if filter item then yield item }
+
+ // A helper
+let toText collection =
+    let mutable x = "";
+    for item in collection do
+        x <- x + " " + item.ToString()
+    x
+
+ // Hard implementation
+let writeToConsole text =
+    printfn "%s" text
+
+// Hard implementation
+let writeToFile text =
+    File.WriteAllText(@"C:\temp\output.txt", text)
+
+// A higher order function
+let log writer x =
+    writer x
+
+[<EntryPoint>]
+let main argv =
+    let customers = [| { age = 12 }; { age = 13 } |]
+
+    // Calling WHERE with the hard implementation inlined.
+    let customersOfThirteenOrOlder =
+        customers |> where (fun customer -> customer.age >= 13)
+
+    // Shovin data to the log-function, defining which implementation to use.
+    customersOfThirteenOrOlder |> toText |> log writeToConsole
+    customersOfThirteenOrOlder |> toText |> log writeToFile
+    0 // return an integer exit code
---
+++ Get Grogramming with F Sharp/05 Lesson 13 Higher Order Functions/Lesson 13 Higher Order Functions/Scratchpad.fsx
@@ -0,0 +1,25 @@
+﻿type Customer = { age : int}
+
+let where filter collection =
+    seq {
+        for item in collection do
+            if filter item then yield item }
+
+let toText collection =
+    let mutable x = "";
+    for item in collection do
+        x <- x + item.ToString()
+    x
+
+let customers = [| { age = 12 }; { age = 13 } |]
+
+let customersOfThirteenOrOlder =
+    customers |> where (fun customer -> customer.age >= 13)
+
+let x = customersOfThirteenOrOlder.ToString()
+
+customersOfThirteenOrOlder |> toText |> printfn "%s"
+
+
+
+
