992e9073e5963e964008bca70c4fa66696f113f8
IzeDev <jimmy.gustafsson@live.com>
2019-02-20 20:12:31 +0100

    Did another lesson

---
+++ Get Grogramming with F Sharp/Lesson 30 Introducing Type providers/Lesson 30 Introducing Type providers/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Data
+
+type FoodItem = CsvProvider< @"D:\Source\F-Sharp Demos\Get Grogramming with F Sharp\00 Lesson data\Livsmedelsdatabasen.csv" >
+
+[<EntryPoint>]
+let main argv =
+    let data = FoodItem.GetSample().Rows |> Seq.toArray
+    let headers = FoodItem.GetSample().Headers;
+    0
