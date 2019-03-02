e75a5d219d9edc03b7f02ef9caa4302c324e4d85
IzeDev <jimmy.gustafsson@live.com>
2019-03-02 16:05:02 +0100

    HTTP-lessons

---
+++ Get Grogramming with F Sharp/18 Lesson 37 Exposing Data Over HTTP/Lesson 37 Exposing Data Over HTTP/Program.fs
@@ -0,0 +1,43 @@
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Newtonsoft.Json
+
+let getAllTheAnimal =
+    [|
+        "Monkey"
+        "Horse"
+        "Dog"
+        "Mouse"
+        "Sparrow"
+        "Snake"
+    |]
+
+let getAnimal strVal iVal =
+    [| for i in 1 .. iVal -> strVal |]
+
+//let app =
+//  GET >=> choose // combines both WebParts
+//       [ path "/hello" >=> OK "Hello GET"
+//         pathScan "/add/%d/%d" (
+//              fun (n1,n2) ->
+//                OK <| sprintf "%d" (n1 + n2)
+//            ) ]
+
+let app =
+  choose
+    [ GET >=> choose
+        [ path "/hello" >=> OK "Hello GET"
+          path "/goodbye" >=> OK "Good bye GET" 
+          path "/animals" >=> OK (getAllTheAnimal |> JsonConvert.SerializeObject) ]
+      POST >=> choose
+        [ path "/hello" >=> OK "Hello POST"
+          path "/goodbye" >=> OK "Good bye POST"
+          pathScan "/animals/%s%i" (fun (q, i) -> OK( getAnimal q i |> JsonConvert.SerializeObject) ) ]
+    ]
+
+[<EntryPoint>]
+let main argv =
+    startWebServer defaultConfig app
+    0
---
+++ Get Grogramming with F Sharp/19 Lesson 38 Consuming HTTP data/Lesson 38 Consuming HTTP data/Program.fs
@@ -0,0 +1,27 @@
+﻿open HttpFs.Client
+open Hopac
+open System
+
+let basePath = "http://127.0.0.1:8080";
+
+//let getAllTheAnimals =
+//    Request.createUrl Get (basePath + "/animals")
+//    |> Request.responseAsString
+//    |> run
+
+let getSomeAnimal =
+    try
+        Request.createUrl Post (basePath + "/animals/")
+        |> Request.queryStringItem "animalname" "m"
+        |> Request.queryStringItem "numberOfThings" "5"
+        |> Request.responseAsString
+        |> run
+    with
+    | Failure(msg) -> Console.WriteLine(msg); msg;
+
+[<EntryPoint>]
+let main argv =
+    //getAllTheAnimals |> printfn "%s"
+    getSomeAnimal |> printfn "%s"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
