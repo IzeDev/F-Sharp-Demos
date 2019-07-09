f172250ab2b7a5b624ab98de216e2f7120366654
IzeDev <jimmy.gustafsson@live.com>
2019-07-09 13:30:16 +0200

    Changes

--- TokenTest/TokenTest/Program.fs
+++ TokenTest/TokenTest/Program.fs
@@ -2,46 +2,41 @@
 open System.Text
 open System
 
-let generateToken x =
+let constructToken (characters : Char []) doConvert =
+    match characters with
+    | [||] -> ""
+    | _ ->
+        match doConvert with
+        | true -> characters |> String.Concat
+        | false ->
+            characters
+                |> Array.map (fun e -> Convert.ToInt64(e).ToString())
+                |> String.Concat
+
+let generateToken seed =
     let randomGenerator = RandomNumberGenerator.Create()
     
-    let mutable myBytes = Array.create 10 0uy
+    let mutable myBytes = Array.create seed 0uy
     randomGenerator.GetBytes(myBytes)    
     
     let characters = Encoding.UTF8.GetChars(myBytes)
 
     let rawTokenCharacters =
-        characters |> Array.groupBy (fun c -> Char.IsDigit c && Char.to)
-
-    let x =
-        if rawTokenCharacters.Length = 1 then
-            rawTokenCharacters
-            |> Array.head
-            |> snd
-            |> Array.map (fun e -> e.ToString())
-            |> String.Concat
-        else        
-            rawTokenCharacters
-                |> Array.filter (fun e -> fst e = false)
-                |> Array.head
-                |> snd // Get rid of the boolean and get the actual array
-                |> Array.map (fun e -> Convert.ToInt64(e).ToString().ToCharArray())
-                |> Array.reduce (fun ele1 ele2 -> Array.append ele1 ele2)
-                |> Array.map (fun e -> e.ToString())
-                |> Array.append (rawTokenCharacters
-                    |> Array.filter (fun e -> fst e = true)
-                    |> Array.head
-                    |> snd // Get rid of the boolean and get the actual array
-                    |> Array.map (fun e -> e.ToString()))
-                |> String.Concat
-
-    printfn "%A" rawTokenCharacters
-    printfn "%s" x
+        characters |> Array.groupBy (fun c ->
+            (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ||
+            (c >= '0' && c <= '9'))
+            
+    if rawTokenCharacters.Length = 1 then
+        constructToken (rawTokenCharacters.[0] |> snd) (rawTokenCharacters.[0] |> fst)
+    else
+        constructToken (rawTokenCharacters.[1] |> snd) (rawTokenCharacters.[1] |> fst) +
+        constructToken (rawTokenCharacters.[0] |> snd) (rawTokenCharacters.[0] |> fst)
 
 [<EntryPoint>]
 let main argv =
     for i in 1 .. 100 do
-        generateToken i
+        Console.Write(generateToken 10)
         Console.WriteLine()
 
     Console.ReadKey() |> ignore
